248a72b66f90cc223dab25813743926318d86e4b
Mamen code <mamencode@gmail.com>
2022-05-07 14:51:45 +0000

    cd

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Blurhash } from 'react-native-blurhash';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Blurhash
+      blurhash="LGFFaXYk^6#M@-5c,1J5@[or[Q6."
+      style={{flex: 1}}
+    />
   );
 }
 
